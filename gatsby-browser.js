7cbcc0163306a83bade7a48d4a2619654d8662aa
Youngwook Kim <youngwook.kim@gmail.com>
2023-08-28 22:34:08 +0900

    feat: integrate Tailwind CSS with PostCSS

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -8,3 +8,6 @@ import "./src/style.css"
 
 // Highlighting for code blocks
 import "prismjs/themes/prism.css"
+
+// Tailwind CSS
+import "./src/styles/global.css"
--- gatsby-config.js
+++ gatsby-config.js
@@ -130,5 +130,6 @@ module.exports = {
         shortname: `ywkim`,
       },
     },
+    "gatsby-plugin-postcss",
   ],
 }
