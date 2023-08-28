b648b7fb323127bc35e79195f5a73d60dd393966
Youngwook Kim <youngwook.kim@gmail.com>
2023-08-29 01:56:52 +0900

    feat: Add TailwindCSS and remove unused styles and fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,4 +9,29 @@ module.exports = {
     extend: {},
   },
   plugins: [],
+  purge: {
+    options: {
+      // Do not remove the following classes when doing the purge
+      // because they are added dynamically in the markdown files
+      safelist: [
+        "text-4xl",
+        "font-bold",
+        "text-gray-900",
+        "mb-4",
+        "mt-8",
+        "text-3xl",
+        "text-2xl",
+        "font-semibold",
+        "text-gray-700",
+        "text-xl",
+        "font-medium",
+        "text-gray-600",
+        "text-lg",
+        "text-gray-500",
+        "text-base",
+        "text-gray-400",
+        "my-6",
+      ],
+    },
+  },
 }
